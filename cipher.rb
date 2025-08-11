cca36bfbfdc8b43f84018fbf2ec12a1fbd934945
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-08-12 05:27:46 +0530

    MODIFY: SYNTAX CORRECTION AND IF CHANGE TO CASE

--- cipher.rb
+++ cipher.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 def ceasar_cipher(string, shift)
-	string.chars.map {|char| shift_char(char, shift)}.join
+  string.chars.map { |char| shift_char(char, shift) }.join
 end
 
 def shift_char(char, shift)
-	case char
-	when'A'..'Z'
-		get_utf_value(char, shift, 'A'.ord)
-	when'a'..'z'
-		get_utf_value(char, shift, 'a'.ord)
-	else
-		char
-	end
+  case char
+  when 'A'..'Z'
+    get_utf_value(char, shift, 'A'.ord)
+  when 'a'..'z'
+    get_utf_value(char, shift, 'a'.ord)
+  else
+    char
+  end
 end
 
 def get_utf_value(char, shift_value, base)
-	(((char.ord - base + shift_value) % 26) + base).chr
+  (((char.ord - base + shift_value) % 26) + base).chr
 end
 
-puts ceasar_cipher("What a string!", 5)
+puts ceasar_cipher('What a string!', 5)
